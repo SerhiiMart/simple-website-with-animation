8503186920b131bac4b7dbaa034d5f05baefb62b
Serhii <malgothsm@gmail.com>
2021-11-16 09:57:25 +0200

    Fixed webpage

--- script.js
+++
@@ -1,7 +0,0 @@
-const slider = gsap.timeline({ defaults: { ease: "power1.out" } });
-
-slider.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-slider.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-slider.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-slider.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
-slider.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
